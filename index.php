14e412647e847d75e91d955693bb2edf76d44d61
Renato Soto <renato.soto@gmail.com>
2017-06-10 22:44:52 -0400

    Create index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php 
-session_start();
+<?php
+/** Lista de Tareas : Solemne 2 **/
+session_start(); // Cuando se trabaja con variables de session siempre debe llevar esta linea
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,30 +8,37 @@ session_start();
     <title>Solemne 2</title>
 </head>
 <body>
-<h1>Mis Tareas</h1>
-<form action="controlador.php" method="post">
-  <label>Tarea: </label>
-  <input type="text" name="tarea" placeholder="Ingrese la tarea" />
-  <input type="hidden" name="action" value="1" />
-  <button type="submit" class="btn btn-sm btn-primary"> + </button>
-</form>
-<?php
-if (isset($_SESSION['tareas'])){
-    foreach (array_keys($_SESSION['tareas']) as $i){
-        ?>
-        <form action="controlador.php" method="post">
-            <input type="hidden" name="action" value="2" />&nbsp;</div>
-            <button type="submit" class="btn"> x </button></div>
-            <?php echo $_SESSION['tareas'][$i]; ?></div>
-            <input type="hidden" name="id" value="<?php echo $i; ?>" />&nbsp;</div>
-        </form>
-        <?php
-        }
-    }
-else{
-    <label> VACIO </label>
+    <h1>Mis Tareas</h1>
+    <form action="controlador.php" method="post">
+        <label>Tarea: </label>
+        <input type="text" name="tarea" required="required" placeholder="Ingrese la tarea" />
+        <button type="submit"> + </button>
+        <input type="hidden" name="action" value="1" />
+    </form>
     <?php
-    }
-    ?>
+        /* Comprueba que exista la variable $_SESSION y que contenga valores */
+        if (isset($_SESSION['tareas'])){ 
+            /* Recorre el Array de la variable de seccion y ademas asigna un contador $i */
+            foreach (array_keys($_SESSION['tareas']) as $i){
+                /* creo un formulario para enviar el valor que deseo eliminar dentro del arreglo */
+                ?>
+                <form action="controlador.php" method="post">
+                    <button type="submit"> x </button>
+                    <?php echo $_SESSION['tareas'][$i]; ?>
+                    <input type="hidden" name="id" value="<?php echo $i; ?>" />
+                    <input type="hidden" name="action" value="2" />
+                </form>
+                <?php
+                }
+            }
+        else{
+            ?>
+            <label>Sin tareas que mostrar</label>
+            <?php
+			}
+        ?>
 </body>
 </html>
+<?php
+/* Autor: Renato Soto */
+?>
