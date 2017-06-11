091d56adb567e960121424e056c9c8706a4609f7
Renato Soto <renato.soto@gmail.com>
2017-06-10 22:52:50 -0400

    Create Tarea.php

--- Tarea.php
+++ Tarea.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
 class Tarea {
+	/* este es el metodo para agregar una tarea al array */
 	public function setArray($lista){
 		$_SESSION['tareas'][]=$lista;
 		}
+	/* metodo para eliminar una posicion del array */
 	public function delPosArray($id){
-		unset($_SESSION['tareas'][$id]);
-		$_SESSION['tareas']=array_values($_SESSION['tareas']);
+		unset($_SESSION['tareas'][$id]); /* con este comando eliminamos una posicion del array */
+		$_SESSION['tareas']=array_values($_SESSION['tareas']); /* con este comando reordenamos las posiciones */
 		}
 	}
